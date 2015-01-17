5a546ac2b2137970c63d4fa7d78c37d7c4b89642
Manuel Kallenbach <mk@manukall.de>
2015-01-17 12:32:21 +0100

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FeederEx.Mixfile do
 
   def project do
     [app: :feeder_ex,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      deps: deps,
      package: package]
