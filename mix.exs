bb28177dcb600bbc7782235f354b8911df9dc95a
Manuel Kallenbach <mk@manukall.de>
2016-09-18 11:14:28 +0200

    version 0.0.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FeederEx.Mixfile do
 
   def project do
     [app: :feeder_ex,
-     version: "0.0.4",
+     version: "0.0.5",
      elixir: "~> 1.0",
      deps: deps,
      package: package]
